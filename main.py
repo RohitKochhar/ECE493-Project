2442be96a101ba3144c44e0bd3e86065927d28ef
Rohit Singh <rkochhar@uwaterloo.ca>
2022-07-23 15:48:36 -0400

    created and tested simple simulation

--- main.py
+++ main.py
@@ -11,8 +11,12 @@
 # Imports --------------------------------------------------------
 
 # Global Variables -----------------------------------------------
+from random import randint
+from time import sleep
+
+
 CAR_LENGTH = 5
-INFINITY = 10000000000000
+INFINITY = 100000000000000000
 
 # Class Declarations ---------------------------------------------
 
@@ -33,6 +37,7 @@ class Node:
         self.edges      = []
         self.lat        = lat
         self.long       = long
+        self.prev       = None
 
     # .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   . 
     #
@@ -47,6 +52,11 @@ class Node:
     # .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   . 
     def setEdge(self, edge):
         self.edges.append(edge)
+
+    def getEdgeBySink(self, sink):
+        for edge in self.edges:
+            if edge.sinkNode == sink:
+                return edge
 
     def __str__(self):
         return f"Node {self.id}"
@@ -82,6 +92,8 @@ class NodeManager:
     #
     # .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   . 
     def determinePath(self, currentNode, targetNode):
+        if currentNode == targetNode:
+            return 0, []
         # Mark all nodes unvisited
         for node in self.nodes:
             node.isVisited = False
@@ -111,12 +123,100 @@ class NodeManager:
                             # Check if a shorter path exists
                             if edge.realTime + current.tentativeDistance <= neighbour.tentativeDistance:
                                 neighbour.tentativeDistance = edge.realTime + current.tentativeDistance
+                                neighbour.prev = current
                 
                 current.isVisited = True
 
+        if targetNode.tentativeDistance == INFINITY:
+            path = []
+        else:
+            lastNode = targetNode
+            path = []
+            path.append(targetNode)
+            while lastNode.prev != currentNode:
+                path.append(lastNode.prev)
+                lastNode = lastNode.prev
+            path.append(currentNode)
 
         print(f"Min path from Node {currentNode.id} -> {targetNode.id} = {targetNode.tentativeDistance/3600} hours")
-        return targetNode.tentativeDistance
+        reversedPath = list(reversed(path))
+        print(f"{' -> '.join([str(x) for x in reversedPath]) }")
+        edges = []
+        for i in range(0, len(reversedPath) - 1):
+            edge = reversedPath[i].getEdgeBySink(reversedPath[i + 1])
+            edges.append(edge)
+            print(edge)
+        return targetNode.tentativeDistance, reversedPath, edges
+
+    # .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   . 
+    #
+    #	Method Name:    determineBaselinePath
+    #
+    #	Method Description:
+    #		-   Run Djikstras from self node to a target node
+    #
+    #	Method History:
+    #		- 2022-07-16: Created by Rohit S.
+    #
+    # .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   . 
+    def determineBaselinePath(self, currentNode, targetNode):
+        if currentNode == targetNode:
+            return 0, []
+        # Mark all nodes unvisited
+        for node in self.nodes:
+            node.isVisited = False
+            # Mark all nodes with a tentative distance of infinity
+            if node != currentNode:
+                node.tentativeDistance = INFINITY
+            else:
+                node.tentativeDistance = 0
+        # Consider all unvisited nodes
+        while all([x.isVisited for x in self.nodes]) != True:
+            # Get remaining unvisted nodes:
+            unvisitedNodes = []
+            for node in self.nodes:
+                if node.isVisited == False:
+                    unvisitedNodes.append(node)
+            # Get the next smallest 
+            current = min(unvisitedNodes, key=lambda x: x.tentativeDistance)
+            if current == targetNode:
+                break
+            else:
+                # Consider the neighbours of current
+                for edge in current.edges:
+                    if current == edge.sourceNode:
+                        neighbour = edge.sinkNode
+                        # Check that the neighbour hasn't been visited
+                        if neighbour in self.nodes:
+                            # Check if a shorter path exists
+                            if edge.realTime + current.tentativeDistance <= neighbour.tentativeDistance:
+                                neighbour.tentativeDistance = edge.minTime + current.tentativeDistance
+                                neighbour.prev = current
+                
+                current.isVisited = True
+
+        if targetNode.tentativeDistance == INFINITY:
+            path    = []
+        else:
+            lastNode = targetNode
+            path = []
+            path.append(targetNode)
+            while lastNode.prev != currentNode:
+                path.append(lastNode.prev)
+                lastNode = lastNode.prev
+            path.append(currentNode)
+
+
+        print(f"Min path from Node {currentNode.id} -> {targetNode.id} = {targetNode.tentativeDistance/3600} hours")
+        reversedPath = list(reversed(path))
+        print(f"{' -> '.join([str(x) for x in reversedPath]) }")
+        edges = []
+        for i in range(0, len(reversedPath) - 1):
+            edge = reversedPath[i].getEdgeBySink(reversedPath[i + 1])
+            edges.append(edge)
+            print(edge)
+        return targetNode.tentativeDistance, reversedPath, edges
+
 
 # . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
 #	Class Name:         Edge
@@ -129,7 +229,7 @@ class NodeManager:
 # 
 # . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
 class Edge:
-    def __init__(self, sourceNode, sinkNode, length=1, maxSpeed=50):
+    def __init__(self, sourceNode, sinkNode, length=100, maxSpeed=50):
         self.sourceNode = sourceNode
         self.sinkNode   = sinkNode
         self.length     = length
@@ -137,7 +237,7 @@ class Edge:
         self.minTime    = (length / maxSpeed) * 3600
         self.numCars    = 0
         self.d          = self.length
-        self.realSpeed  = 0
+        self.realSpeed  = self.setRealSpeed()
         self.congestion = 1
 
         self.sourceNode.setEdge(self)
@@ -209,7 +309,11 @@ class Edge:
         self.setCongestion()
 
     def __str__(self):
+        return f"edge{self.sourceNode.id}{self.sinkNode.id}"
+
+    def getEdgeInfo(self):
         return f"Edge information: \n\tCars: {self.numCars}, d: {self.d} \n\tIdeal speed: {self.maxSpeed}, Real speed: {self.realSpeed}\n\tMin time: {self.minTime}, Real time: {self.realTime}\n\tCongestion: {self.congestion}"
+
 
 # . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
 #	Class Name:     EdgeManager
@@ -225,11 +329,98 @@ class EdgeManager:
     def __init__(self):
         self.edges = []
     
-    def createEdge(self, sourceNode, sinkNode, length=1, maxSpeed=50):
+    def createEdge(self, sourceNode, sinkNode, length=100, maxSpeed=50):
         edge = Edge(sourceNode, sinkNode, length, maxSpeed)
         self.edges.append(edge)
         return edge
 
+    def createBidirectionalEdge(self, nodeA, nodeB, length=1, maxSpeed=50):
+        self.createEdge(nodeA, nodeB, length, maxSpeed)
+        self.createEdge(nodeB, nodeA, length, maxSpeed)
+
+    def getEdgeFromNodes(self, sourceNode, sinkNode):
+        for edge in self.edges:
+            if (edge.sourceNode == sourceNode) and (edge.sinkNode == sinkNode):
+                return edge
+
+class Vehicle:
+    def __init__(self, startNode, endNode, nodeManager):
+        self.startNode      = startNode
+        self.endNode        = endNode
+        self.baselinePath   = nodeManager.determineBaselinePath(self.startNode, self.endNode)
+        timeLeftOnEdge      = INFINITY
+
+    def updateEdge(self):
+        for i in range(0, len(self.edges)):
+            if self.edges[i] == self.currentEdge:
+                try:
+                    self.nextEdge = self.edges[i + 1]
+                    self.currentEdge = self.nextEdge
+                    self.timeLeftOnEdge = self.currentEdge.minTime
+                except IndexError as e:
+                    self.nextEdge = None    
+                    self.currentEdge = self.nextEdge
+
+
+# . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+#	Class Name: Simulator
+#
+#	Class Description: 
+#       - Implements the discrete event simulator
+# 
+#	Class History: 
+# 		- 2022-07-18: Created by Rohit S.
+# 
+# . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+class Simulator:
+    def __init__(self, nodeManager, edgeManager):
+        self.nodeManager    = nodeManager
+        self.edgeManager    = edgeManager
+        self.vehicles       = []
+
+    def addRandomVehicle(self):
+        numNodes = len(self.nodeManager.nodes)
+        startNode   = self.nodeManager.nodes[randint(0, numNodes - 1)]
+        endNode     = self.nodeManager.nodes[randint(0, numNodes - 1)]
+        while endNode == startNode:
+            endNode     = self.nodeManager.nodes[randint(0, numNodes - 1)]
+        vehicle     = Vehicle(startNode, endNode, self.nodeManager)
+        self.vehicles.append(vehicle)
+
+    def startSimulation(self):
+        print(f"-------------------------------")
+        print(f"Starting Simulation...")
+        print(f"\tNumber of Vehicles: {len(self.vehicles)}")
+        for i in range(0, len(self.vehicles)):
+            print(f"\t\tVehicle {i} travelling from {self.vehicles[i].startNode} to {self.vehicles[i].endNode}")
+            minTime, minPath, minEdges = self.vehicles[i].baselinePath
+            print(f"\t\t\tBaseline Path: {' -> '.join(str(node) for node in minPath)}")
+            print(f"\t\t\tStarting on edge: {minEdges[0]}")
+            self.vehicles[i].currentEdge = minEdges[0]
+            self.vehicles[i].edges       = minEdges
+            print(f"\t\t\tTime remaining on edge: {minEdges[0].minTime} seconds")
+            self.vehicles[i].timeLeftOnEdge = minEdges[0].minTime
+        step = 0
+        while len(self.vehicles) != 0:
+            print(f"- - - - - Simulation Step {step} - - - - -")
+            self.updateSimulation()
+            step += 1
+
+    def updateSimulation(self):
+        for vehicle in self.vehicles:
+            print(f"Vehicle on edge: {vehicle.currentEdge}")
+            vehicle.timeLeftOnEdge -= 1
+            if vehicle.timeLeftOnEdge <= 0:
+                print(f"\tCompleted Trip on edge")
+                vehicle.updateEdge()
+                if vehicle.currentEdge == None:
+                    self.vehicles.remove(vehicle)
+                    del(vehicle)
+                    print(len(self.vehicles))
+            else:
+                print(f"\tTime left on edge: {vehicle.timeLeftOnEdge}")
+
+        
 # Function Declarations ------------------------------------------
 
 # Main Call ------------------------------------------------------
--- test_main.py
+++ test_main.py
@@ -10,10 +10,10 @@
 # . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
 
 # Imports --------------------------------------------------------
-from main import EdgeManager, Node, Edge, NodeManager
+from main import EdgeManager, Node, Edge, NodeManager, Simulator
 
 # Global Variables -----------------------------------------------
-INFINITY = 10000000000000
+INFINITY = 100000000000000000
 
 # Class Declarations ---------------------------------------------
 
@@ -85,10 +85,8 @@ def test_simple_djikstras():
     sinkNode    = nodeManager.createNode(0, 0)
     # Add an edge
     edge        = edgeManager.createEdge(sourceNode, sinkNode, 5000, 100)
-    # Add a vehicle
-    edge.addVehicle()
     # Get min path
-    minPath = nodeManager.determinePath(sourceNode, sinkNode)
+    minPath, path, edges = nodeManager.determinePath(sourceNode, sinkNode)
     assert minPath == edge.realTime
 
 def test_double_djikstras():
@@ -101,12 +99,9 @@ def test_double_djikstras():
     sinkNode    = nodeManager.createNode(0, 0)
     # Add edges
     edge1       = edgeManager.createEdge(sourceNode, interNode, 5000, 100)
-    edge2       = edgeManager.createEdge(interNode, sinkNode, 1000, 100)
-    # Add vehicles
-    edge1.addVehicle()
-    edge2.addVehicle()    
+    edge2       = edgeManager.createEdge(interNode, sinkNode, 1000, 100)  
     # Get min path
-    minPath = nodeManager.determinePath(sourceNode, sinkNode)
+    minPath, path, edges = nodeManager.determinePath(sourceNode, sinkNode)
     assert minPath == edge1.realTime + edge2.realTime
     
 def test_djikstras_with_trick_double_path():
@@ -121,12 +116,8 @@ def test_djikstras_with_trick_double_path():
     edge1       = edgeManager.createEdge(sourceNode, interNode, 5000, 100)
     edge2       = edgeManager.createEdge(interNode, sinkNode, 1000, 100)
     edge3       = edgeManager.createEdge(sourceNode, sinkNode, 1000000, 100)
-    # Add vehicles
-    edge1.addVehicle()
-    edge2.addVehicle()    
-    edge3.addVehicle()
     # Get min path
-    minPath = nodeManager.determinePath(sourceNode, sinkNode)
+    minPath, path, edges = nodeManager.determinePath(sourceNode, sinkNode)
     assert minPath == edge1.realTime + edge2.realTime
 
 def test_djikstras_with_trick_single_path():
@@ -141,12 +132,8 @@ def test_djikstras_with_trick_single_path():
     edge1       = edgeManager.createEdge(sourceNode, interNode, 5000, 100)
     edge2       = edgeManager.createEdge(interNode, sinkNode, 1000, 100)
     edge3       = edgeManager.createEdge(sourceNode, sinkNode, 10, 100)
-    # Add vehicles
-    edge1.addVehicle()
-    edge2.addVehicle()    
-    edge3.addVehicle()
     # Get min path
-    minPath = nodeManager.determinePath(sourceNode, sinkNode)
+    minPath, path, edges = nodeManager.determinePath(sourceNode, sinkNode)
     assert minPath == edge3.realTime
 
 def test_djikstras_no_path():
@@ -162,7 +149,7 @@ def test_djikstras_no_path():
     # Add a vehicle
     edge.addVehicle()
     # Get min path
-    minPath = nodeManager.determinePath(sourceNode, sinkNode)
+    minPath, path, edges = nodeManager.determinePath(sourceNode, sinkNode)
     assert minPath == INFINITY
 
 def test_complex_djikstras():
@@ -181,34 +168,131 @@ def test_complex_djikstras():
     node8 = nodeManager.createNode(0, 0)
     # Connect edges
     edge01 = edgeManager.createEdge(node0, node1, 400, 1)
-    edge01.addVehicle()
     edge06 = edgeManager.createEdge(node0, node6, 700, 1)
-    edge06.addVehicle()
     edge16 = edgeManager.createEdge(node1, node6, 400, 1)
-    edge16.addVehicle()
     edge67 = edgeManager.createEdge(node6, node7, 100, 1) 
-    edge67.addVehicle()
     edge17 = edgeManager.createEdge(node1, node7, 2000, 1)
-    edge17.addVehicle()
     edge74 = edgeManager.createEdge(node7, node4, 100, 1)
-    edge74.addVehicle()
     edge12 = edgeManager.createEdge(node1, node2, 900, 1)
-    edge12.addVehicle()
     edge42 = edgeManager.createEdge(node4, node2, 200, 1)
-    edge42.addVehicle()
     edge23 = edgeManager.createEdge(node2, node3, 600, 1)
-    edge23.addVehicle()
     edge43 = edgeManager.createEdge(node4, node3, 1000, 1)
-    edge43.addVehicle()
 
-
-
-
-    assert nodeManager.determinePath(node0, node1) == 400 * 3600
-    assert nodeManager.determinePath(node0, node6) == 700 * 3600
-    assert nodeManager.determinePath(node0, node7) == 800 * 3600
-    assert nodeManager.determinePath(node0, node4) == 900 * 3600
-    assert nodeManager.determinePath(node0, node2) == 1100 * 3600
-    assert nodeManager.determinePath(node0, node3) == 1700 * 3600
+    assert nodeManager.determinePath(node0, node1)[0] == 400 * 3600
+    assert nodeManager.determinePath(node0, node6)[0] == 700 * 3600
+    assert nodeManager.determinePath(node0, node7)[0] == 800 * 3600
+    assert nodeManager.determinePath(node0, node4)[0] == 900 * 3600
+    assert nodeManager.determinePath(node0, node2)[0] == 1100 * 3600
+    assert nodeManager.determinePath(node0, node3)[0] == 1700 * 3600
     
 # ToDo: Test with bi-directional paths
+
+def test_simple_bidirectional_network():
+    # Create managers
+    nodeManager = NodeManager()
+    edgeManager = EdgeManager()
+    # Create three nodes
+    sourceNode  = nodeManager.createNode(0, 0)
+    interNode   = nodeManager.createNode(0, 0)
+    sinkNode    = nodeManager.createNode(0, 0)
+    # Add edges
+    edge1       = edgeManager.createEdge(sourceNode, interNode, 5000, 100)
+    edge2       = edgeManager.createEdge(interNode, sinkNode, 1000, 100)
+    edge3       = edgeManager.createEdge(interNode, sourceNode, 100, 10)
+    # Get min path
+    minPath, path, edges = nodeManager.determinePath(sourceNode, sinkNode)
+    assert minPath == edge1.realTime + edge2.realTime
+
+def test_simple_simulation():
+    # Create managers
+    nodeManager = NodeManager()
+    edgeManager = EdgeManager()
+    # Create nodes
+    sourceNode  = nodeManager.createNode(0, 0)
+    sinkNode    = nodeManager.createNode(0, 0)
+    # Add edges
+    edge        = edgeManager.createBidirectionalEdge(sourceNode, sinkNode, 5000, 100)
+    # Create simulator
+    sim = Simulator(nodeManager, edgeManager)
+    sim.addRandomVehicle()
+    sim.startSimulation()
+
+def test_network_1():
+    greenLength     = 40
+    greenSpeed      = 110
+    yellowLength    = 20
+    yellowSpeed     = 80
+    redLength       = 10
+    redSpeed        = 30
+    # Create managers
+    nodeManager = NodeManager()
+    edgeManager = EdgeManager()
+    # Create nodes
+    nodeA       = nodeManager.createNode(0, 0)
+    nodeB       = nodeManager.createNode(0, 0)
+    nodeC       = nodeManager.createNode(0, 0)
+    nodeD       = nodeManager.createNode(0, 0)
+    nodeE       = nodeManager.createNode(0, 0)
+    nodeF       = nodeManager.createNode(0, 0)
+    nodeG       = nodeManager.createNode(0, 0)
+    nodeH       = nodeManager.createNode(0, 0)
+    nodeI       = nodeManager.createNode(0, 0)
+    nodeJ       = nodeManager.createNode(0, 0)
+    nodeK       = nodeManager.createNode(0, 0)
+    nodeL       = nodeManager.createNode(0, 0)
+    nodeM       = nodeManager.createNode(0, 0)
+    nodeN       = nodeManager.createNode(0, 0)
+    nodeO       = nodeManager.createNode(0, 0)
+    nodeP       = nodeManager.createNode(0, 0)
+    # Create edges
+    #   Highway edges
+    edgeAB      = edgeManager.createBidirectionalEdge(nodeA, nodeB, greenLength, greenSpeed)
+    edgeAO      = edgeManager.createBidirectionalEdge(nodeA, nodeO, greenLength, greenSpeed)
+    edgeBP      = edgeManager.createBidirectionalEdge(nodeB, nodeP, greenLength, greenSpeed)
+    edgeOP      = edgeManager.createBidirectionalEdge(nodeO, nodeO, greenLength, greenSpeed)
+    #   Rural edges
+    edgeAD      = edgeManager.createBidirectionalEdge(nodeA, nodeD, yellowLength, yellowSpeed)
+    edgeAH      = edgeManager.createBidirectionalEdge(nodeA, nodeH, yellowLength, yellowSpeed)
+    edgeBD      = edgeManager.createBidirectionalEdge(nodeB, nodeD, yellowLength, yellowSpeed)
+    edgeBK      = edgeManager.createBidirectionalEdge(nodeB, nodeK, yellowLength, yellowSpeed)
+    edgeOH      = edgeManager.createBidirectionalEdge(nodeO, nodeH, yellowLength, yellowSpeed)
+    edgeOM      = edgeManager.createBidirectionalEdge(nodeO, nodeM, yellowLength, yellowSpeed)
+    edgePM      = edgeManager.createBidirectionalEdge(nodeP, nodeM, yellowLength, yellowSpeed)
+    edgePK      = edgeManager.createBidirectionalEdge(nodeP, nodeK, yellowLength, yellowSpeed)
+    #   City edges
+    edgeAC      = edgeManager.createBidirectionalEdge(nodeA, nodeC, redLength, redSpeed)
+    edgeEB      = edgeManager.createBidirectionalEdge(nodeE, nodeB, redLength, redSpeed)
+    edgeNP      = edgeManager.createBidirectionalEdge(nodeN, nodeP, redLength, redSpeed)
+    edgeOL      = edgeManager.createBidirectionalEdge(nodeO, nodeL, redLength, redSpeed)
+
+    edgeCD      = edgeManager.createBidirectionalEdge(nodeC, nodeD, redLength, redSpeed)
+    edgeDE      = edgeManager.createBidirectionalEdge(nodeD, nodeE, redLength, redSpeed)
+    edgeEK      = edgeManager.createBidirectionalEdge(nodeE, nodeK, redLength, redSpeed)
+    edgeKN      = edgeManager.createBidirectionalEdge(nodeK, nodeN, redLength, redSpeed)
+    edgeNM      = edgeManager.createBidirectionalEdge(nodeN, nodeM, redLength, redSpeed)
+    edgeML      = edgeManager.createBidirectionalEdge(nodeM, nodeL, redLength, redSpeed)
+    edgeLH      = edgeManager.createBidirectionalEdge(nodeL, nodeH, redLength, redSpeed)
+    edgeCH      = edgeManager.createBidirectionalEdge(nodeC, nodeH, redLength, redSpeed)
+    
+    edgeCF      = edgeManager.createBidirectionalEdge(nodeC, nodeF, redLength, redSpeed)
+    edgeFD      = edgeManager.createBidirectionalEdge(nodeF, nodeD, redLength, redSpeed)
+    edgeDG      = edgeManager.createBidirectionalEdge(nodeD, nodeG, redLength, redSpeed)
+    edgeGE      = edgeManager.createBidirectionalEdge(nodeG, nodeE, redLength, redSpeed)
+    edgeGK      = edgeManager.createBidirectionalEdge(nodeG, nodeK, redLength, redSpeed)
+    edgeKJ      = edgeManager.createBidirectionalEdge(nodeK, nodeJ, redLength, redSpeed)
+    edgeJN      = edgeManager.createBidirectionalEdge(nodeJ, nodeN, redLength, redSpeed)
+    edgeJM      = edgeManager.createBidirectionalEdge(nodeJ, nodeM, redLength, redSpeed)
+    edgeMI      = edgeManager.createBidirectionalEdge(nodeM, nodeI, redLength, redSpeed)
+    edgeIL      = edgeManager.createBidirectionalEdge(nodeI, nodeL, redLength, redSpeed)
+    edgeIH      = edgeManager.createBidirectionalEdge(nodeI, nodeH, redLength, redSpeed)
+    edgeHF      = edgeManager.createBidirectionalEdge(nodeH, nodeL, redLength, redSpeed)
+
+    edgeFG      = edgeManager.createBidirectionalEdge(nodeF, nodeG, redLength, redSpeed)
+    edgeGJ      = edgeManager.createBidirectionalEdge(nodeG, nodeJ, redLength, redSpeed)
+    edgeJI      = edgeManager.createBidirectionalEdge(nodeJ, nodeI, redLength, redSpeed)
+    edgeIF      = edgeManager.createBidirectionalEdge(nodeI, nodeF, redLength, redSpeed)
+
+    edgeJF      = edgeManager.createBidirectionalEdge(nodeJ, nodeF, redLength, redSpeed)
+    edgeGI      = edgeManager.createBidirectionalEdge(nodeG, nodeI, redLength, redSpeed)
+
+    simulator = Simulator(nodeManager, edgeManager)
